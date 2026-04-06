9a35e26427e3f5cc4ba84287a389b50db2a542a9
khailashc54-design <khailashc54@gmail.com>
2026-04-06 21:29:18 +0530

    Create union.sv

---
+++ data types/union.sv
@@ -0,0 +1,36 @@
+module tb;
+  union {bit [31:0]a ;bit[3:0][7:0] b;}y;
+  initial begin 
+    //little endian
+    y.a=32'h87654321;
+    $display("value=%0h",y.a);
+    foreach(y.b[i])
+      $display("value[%0d]=%0h",i,y.b[i]);
+    y.b[2]=8'hff;
+    y.b[0]=8'hcc;
+    $display("total=%0h",y.a);
+  end
+  initial begin
+    //big endian 
+    y.a=32'h13141516;
+    $display("value=%0h",y.a);
+    foreach(y.b[i])
+      $display("value[%0d]=%0h",i,y.b[i]);
+    y.b[2]=8'hff;
+    y.b[0]=8'hcc;
+    $display("total=%0h",y.a);
+  end
+endmodule
+//output
+value=87654321
+value[3]=87
+value[2]=65
+value[1]=43
+value[0]=21
+total=87ff43cc
+value=13141516
+value[3]=13
+value[2]=14
+value[1]=15
+value[0]=16
+total=13ff15cc
