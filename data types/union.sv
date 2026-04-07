2a0592ba8f7a2931dc6b2d718027f630ffa4105e
khailashc54-design <khailashc54@gmail.com>
2026-04-07 11:29:14 +0530

    endianness
    Added a new union 'h' with bit fields and updated the big endian section to use it instead of 'y'. Adjusted display outputs accordingly.

--- data types/union.sv
+++ data types/union.sv
@@ -1,5 +1,6 @@
 module tb;
   union {bit [31:0]a ;bit[3:0][7:0] b;}y;
+  union{bit[31:0]k;bit[0:3][7:0]s;}h;
   initial begin 
     //little endian
     y.a=32'h87654321;
@@ -12,13 +13,13 @@ module tb;
   end
   initial begin
     //big endian 
-    y.a=32'h13141516;
-    $display("value=%0h",y.a);
-    foreach(y.b[i])
-      $display("value[%0d]=%0h",i,y.b[i]);
-    y.b[2]=8'hff;
-    y.b[0]=8'hcc;
-    $display("total=%0h",y.a);
+    h.k=32'h13141516;
+    $display("value=%0h",h.k);
+    foreach(h.s[i])
+      $display("value[%0d]=%0h",i,h.s[i]);
+    h.s[2]=8'hff;
+    h.s[0]=8'hcc;
+    $display("total=%0h",h.s);
   end
 endmodule
 //output
@@ -29,8 +30,8 @@ value[1]=43
 value[0]=21
 total=87ff43cc
 value=13141516
-value[3]=13
-value[2]=14
-value[1]=15
-value[0]=16
-total=13ff15cc
+value[0]=13
+value[1]=14
+value[2]=15
+value[3]=16
+total=cc14ff16
