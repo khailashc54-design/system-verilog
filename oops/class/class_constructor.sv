ac86b5a43d93fe01fa5fd6f336793515260853ad
khailashc54-design <khailashc54@gmail.com>
2026-04-09 18:37:48 +0530

    Add arithmetic class with constructor and display method

---
+++ oops/class/class_constructor.sv
@@ -0,0 +1,23 @@
+class arthemetic;
+   int a,b,c;
+   function new(int a=0,int b=0);
+     this.a=a;
+     this.b=b;
+     c=a+b;
+   endfunction
+   function display();
+     $display("value=%0d",c);
+   endfunction
+ endclass
+// tb
+  module tb;
+  arthemetic a1,a2;
+  initial begin
+    a1=new();
+    a2=a1;
+    a2.display();
+    a1=new(3,4);
+    a2.display();
+    a1.display();
+  end
+endmodule
