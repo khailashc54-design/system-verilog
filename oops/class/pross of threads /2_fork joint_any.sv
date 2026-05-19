6f1ce0933a7d939d98e692c8dddb6e522c03ae98
khailashc54-design <khailashc54@gmail.com>
2026-05-19 15:34:33 +0530

    Add SystemVerilog testbench with fork and join_any

---
+++ oops/class/pross of threads /2_fork joint_any.sv
@@ -0,0 +1,38 @@
+module tb;
+  initial begin 
+  fork
+    
+   begin 
+      
+       $display("$time=%0t,dhoni",$time);
+       #10;
+       $display("$time=%0t,raina",$time);
+  end
+    begin 
+      
+      $display("$time=%0t,jadeja",$time);
+       #20;
+      $display("$time=%0t,hitman",$time);
+  end
+     begin 
+      
+      $display("$time=%0t,virat kholi",$time);
+       #30;
+      $display("$time=%0t,kailash ",$time);
+  end
+  join_any
+    $display("$time=%0t,dube",$time);
+ #20   $display("$time=%0t,dube",$time);
+    
+  
+    end
+endmodule 
+//output  
+$time=0,dhoni
+$time=0,jadeja
+$time=0,virat kholi
+$time=10,raina
+$time=10,dube
+$time=20,hitman
+$time=30,kailash 
+$time=30,dube
