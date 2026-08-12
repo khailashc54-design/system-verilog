80d8f0ca72dd5a8131f52ade37ec4e607b7f9ba2
khailashc54-design <khailashc54@gmail.com>
2026-08-12 11:00:54 +0530

    Update output and count results in randomization module
    Added output results and occurrence counts for randomization.

--- oops/class/randomization_method/randomization with constrain /weighted constraint/1_using(:=).sv
+++ oops/class/randomization_method/randomization with constrain /weighted constraint/1_using(:=).sv
@@ -25,3 +25,69 @@ module tb;
   end
 
 endmodule
+//=========================output========================\\
+a = 12
+a = 3
+a = 3
+a = 1
+a = 0
+a = 7
+a = 2
+a = 15
+a = 0
+a = 1
+a = 3
+a = 0
+a = 0
+a = 5
+a = 5
+a = 1
+a = 6
+a = 3
+a = 3
+a = 0
+a = 9
+a = 3
+a = 7
+a = 12
+a = 6
+a = 1
+a = 14
+a = 0
+a = 8
+a = 3
+a = 7
+a = 12
+a = 0
+a = 9
+a = 8
+a = 0
+a = 6
+a = 1
+a = 3
+a = 2
+a = 1
+a = 0
+a = 3
+a = 9
+a = 0
+a = 2
+a = 9
+a = 8
+a = 1
+a = 1
+------------- COUNT -------------
+Number 0 occurred 10 times
+Number 1 occurred 8 times
+Number 2 occurred 3 times
+Number 3 occurred 9 times
+Number 5 occurred 2 times
+Number 6 occurred 3 times
+Number 7 occurred 3 times
+Number 8 occurred 3 times
+Number 9 occurred 4 times
+Number 12 occurred 3 times
+Number 14 occurred 1 times
+Number 15 occurred 1 times
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
