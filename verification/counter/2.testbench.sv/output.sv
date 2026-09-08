e961ead0d9978b4d97e9e66f3bccbbb366361f18
khailashc54-design <khailashc54@gmail.com>
2026-09-08 11:09:59 +0530

    Add simulation output for counter testbench
    The output file contains simulation results for the counter testbench, showing expected versus actual output at various time intervals. All test cases passed with a total count of 30.

---
+++ verification/counter/2.testbench.sv/output.sv
@@ -0,0 +1,202 @@
+TOOL:	xrun	25.03-s001: Started on Sep 08, 2026 at 01:37:27 EDT
+xrun: 25.03-s001: (c) Copyright 1995-2025 Cadence Design Systems, Inc.
+	Top level design units:
+		$unit_0x67f934e9
+		top_tb
+Loading snapshot worklib.top_tb:sv .................... Done
+SVSEED default: 1
+xcelium> source /xcelium25.03/tools/xcelium/files/xmsimrc
+xcelium> run
+[GENERATOR]|TIME=0|clk=1 | rst=0
+[MONITOR ]|TIME=0|clk=1 | rst=0 |cout=0
+[SCOREBOARD] Time=0 | rst=0
+expected=0 cout=0
+COUNTER PASS
+[GENERATOR]|TIME=10|clk=1 | rst=1
+[MONITOR ]|TIME=10|clk=1 | rst=0 |cout=1
+[DRIVER]|TIME=10|clk=1 | rst=0
+[SCOREBOARD] Time=10 | rst=0
+expected=1 cout=1
+COUNTER PASS
+[GENERATOR]|TIME=20|clk=1 | rst=0
+[MONITOR ]|TIME=20|clk=1 | rst=1 |cout=0
+[DRIVER]|TIME=20|clk=1 | rst=1
+RST DECTECTED
+[SCOREBOARD] Time=20 | rst=1
+expected=0 cout=0
+COUNTER PASS
+[GENERATOR]|TIME=30|clk=1 | rst=0
+[MONITOR ]|TIME=30|clk=1 | rst=0 |cout=1
+[DRIVER]|TIME=30|clk=1 | rst=0
+[SCOREBOARD] Time=30 | rst=0
+expected=1 cout=1
+COUNTER PASS
+[GENERATOR]|TIME=40|clk=1 | rst=0
+[MONITOR ]|TIME=40|clk=1 | rst=0 |cout=2
+[DRIVER]|TIME=40|clk=1 | rst=0
+[SCOREBOARD] Time=40 | rst=0
+expected=2 cout=2
+COUNTER PASS
+[GENERATOR]|TIME=50|clk=1 | rst=0
+[MONITOR ]|TIME=50|clk=1 | rst=0 |cout=3
+[DRIVER]|TIME=50|clk=1 | rst=0
+[SCOREBOARD] Time=50 | rst=0
+expected=3 cout=3
+COUNTER PASS
+[GENERATOR]|TIME=60|clk=1 | rst=1
+[MONITOR ]|TIME=60|clk=1 | rst=0 |cout=4
+[DRIVER]|TIME=60|clk=1 | rst=0
+[SCOREBOARD] Time=60 | rst=0
+expected=4 cout=4
+COUNTER PASS
+[GENERATOR]|TIME=70|clk=1 | rst=0
+[MONITOR ]|TIME=70|clk=1 | rst=1 |cout=0
+[DRIVER]|TIME=70|clk=1 | rst=1
+RST DECTECTED
+[SCOREBOARD] Time=70 | rst=1
+expected=0 cout=0
+COUNTER PASS
+[GENERATOR]|TIME=80|clk=1 | rst=0
+[MONITOR ]|TIME=80|clk=1 | rst=0 |cout=1
+[DRIVER]|TIME=80|clk=1 | rst=0
+[SCOREBOARD] Time=80 | rst=0
+expected=1 cout=1
+COUNTER PASS
+[GENERATOR]|TIME=90|clk=1 | rst=0
+[MONITOR ]|TIME=90|clk=1 | rst=0 |cout=2
+[DRIVER]|TIME=90|clk=1 | rst=0
+[SCOREBOARD] Time=90 | rst=0
+expected=2 cout=2
+COUNTER PASS
+[GENERATOR]|TIME=100|clk=1 | rst=0
+[MONITOR ]|TIME=100|clk=1 | rst=0 |cout=3
+[DRIVER]|TIME=100|clk=1 | rst=0
+[SCOREBOARD] Time=100 | rst=0
+expected=3 cout=3
+COUNTER PASS
+[GENERATOR]|TIME=110|clk=1 | rst=0
+[MONITOR ]|TIME=110|clk=1 | rst=0 |cout=4
+[DRIVER]|TIME=110|clk=1 | rst=0
+[SCOREBOARD] Time=110 | rst=0
+expected=4 cout=4
+COUNTER PASS
+[GENERATOR]|TIME=120|clk=1 | rst=0
+[MONITOR ]|TIME=120|clk=1 | rst=0 |cout=5
+[DRIVER]|TIME=120|clk=1 | rst=0
+[SCOREBOARD] Time=120 | rst=0
+expected=5 cout=5
+COUNTER PASS
+[GENERATOR]|TIME=130|clk=1 | rst=1
+[MONITOR ]|TIME=130|clk=1 | rst=0 |cout=6
+[DRIVER]|TIME=130|clk=1 | rst=0
+[SCOREBOARD] Time=130 | rst=0
+expected=6 cout=6
+COUNTER PASS
+[GENERATOR]|TIME=140|clk=1 | rst=0
+[MONITOR ]|TIME=140|clk=1 | rst=1 |cout=0
+[DRIVER]|TIME=140|clk=1 | rst=1
+RST DECTECTED
+[SCOREBOARD] Time=140 | rst=1
+expected=0 cout=0
+COUNTER PASS
+[GENERATOR]|TIME=150|clk=1 | rst=0
+[MONITOR ]|TIME=150|clk=1 | rst=0 |cout=1
+[DRIVER]|TIME=150|clk=1 | rst=0
+[SCOREBOARD] Time=150 | rst=0
+expected=1 cout=1
+COUNTER PASS
+[GENERATOR]|TIME=160|clk=1 | rst=0
+[MONITOR ]|TIME=160|clk=1 | rst=0 |cout=2
+[DRIVER]|TIME=160|clk=1 | rst=0
+[SCOREBOARD] Time=160 | rst=0
+expected=2 cout=2
+COUNTER PASS
+[GENERATOR]|TIME=170|clk=1 | rst=0
+[MONITOR ]|TIME=170|clk=1 | rst=0 |cout=3
+[DRIVER]|TIME=170|clk=1 | rst=0
+[SCOREBOARD] Time=170 | rst=0
+expected=3 cout=3
+COUNTER PASS
+[GENERATOR]|TIME=180|clk=1 | rst=0
+[MONITOR ]|TIME=180|clk=1 | rst=0 |cout=4
+[DRIVER]|TIME=180|clk=1 | rst=0
+[SCOREBOARD] Time=180 | rst=0
+expected=4 cout=4
+COUNTER PASS
+[GENERATOR]|TIME=190|clk=1 | rst=0
+[MONITOR ]|TIME=190|clk=1 | rst=0 |cout=5
+[DRIVER]|TIME=190|clk=1 | rst=0
+[SCOREBOARD] Time=190 | rst=0
+expected=5 cout=5
+COUNTER PASS
+[GENERATOR]|TIME=200|clk=1 | rst=0
+[MONITOR ]|TIME=200|clk=1 | rst=0 |cout=6
+[DRIVER]|TIME=200|clk=1 | rst=0
+[SCOREBOARD] Time=200 | rst=0
+expected=6 cout=6
+COUNTER PASS
+[GENERATOR]|TIME=210|clk=1 | rst=0
+[MONITOR ]|TIME=210|clk=1 | rst=0 |cout=7
+[DRIVER]|TIME=210|clk=1 | rst=0
+[SCOREBOARD] Time=210 | rst=0
+expected=7 cout=7
+COUNTER PASS
+[GENERATOR]|TIME=220|clk=1 | rst=0
+[MONITOR ]|TIME=220|clk=1 | rst=0 |cout=8
+[DRIVER]|TIME=220|clk=1 | rst=0
+[SCOREBOARD] Time=220 | rst=0
+expected=8 cout=8
+COUNTER PASS
+[GENERATOR]|TIME=230|clk=1 | rst=0
+[MONITOR ]|TIME=230|clk=1 | rst=0 |cout=9
+[DRIVER]|TIME=230|clk=1 | rst=0
+[SCOREBOARD] Time=230 | rst=0
+expected=9 cout=9
+COUNTER PASS
+[GENERATOR]|TIME=240|clk=1 | rst=0
+[MONITOR ]|TIME=240|clk=1 | rst=0 |cout=10
+[DRIVER]|TIME=240|clk=1 | rst=0
+[SCOREBOARD] Time=240 | rst=0
+expected=10 cout=10
+COUNTER PASS
+[GENERATOR]|TIME=250|clk=1 | rst=1
+[MONITOR ]|TIME=250|clk=1 | rst=0 |cout=11
+[DRIVER]|TIME=250|clk=1 | rst=0
+[SCOREBOARD] Time=250 | rst=0
+expected=11 cout=11
+COUNTER PASS
+[GENERATOR]|TIME=260|clk=1 | rst=0
+[MONITOR ]|TIME=260|clk=1 | rst=1 |cout=0
+[DRIVER]|TIME=260|clk=1 | rst=1
+RST DECTECTED
+[SCOREBOARD] Time=260 | rst=1
+expected=0 cout=0
+COUNTER PASS
+[GENERATOR]|TIME=270|clk=1 | rst=0
+[MONITOR ]|TIME=270|clk=1 | rst=0 |cout=1
+[DRIVER]|TIME=270|clk=1 | rst=0
+[SCOREBOARD] Time=270 | rst=0
+expected=1 cout=1
+COUNTER PASS
+[GENERATOR]|TIME=280|clk=1 | rst=0
+[MONITOR ]|TIME=280|clk=1 | rst=0 |cout=2
+[DRIVER]|TIME=280|clk=1 | rst=0
+[SCOREBOARD] Time=280 | rst=0
+expected=2 cout=2
+COUNTER PASS
+[GENERATOR]|TIME=290|clk=1 | rst=0
+RAND COUNT=30
+[MONITOR ]|TIME=290|clk=1 | rst=0 |cout=3
+[DRIVER]|TIME=290|clk=1 | rst=0
+[SCOREBOARD] Time=290 | rst=0
+expected=3 cout=3
+COUNTER PASS
+PASS COUNT=30
+FAILCOUNT=0
+RST COUNT=4
+TOTAL COUNT=30
+ALL TEST CASED PASED
+[DRIVER]|TIME=300|clk=1 | rst=0
+Simulation complete via $finish(1) at time 320 NS + 0
+./testbench.sv:22     #320;$finish;
+xcelium> exit
